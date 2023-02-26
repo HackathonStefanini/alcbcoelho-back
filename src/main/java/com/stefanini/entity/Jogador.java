da5ab0a9d38b475101c33eb74de27529d2b2d07f
André <alcbcoelho@gmail.com>
2023-02-26 16:42:10 -0300

    Consertados bugs

--- src/main/java/com/stefanini/entity/Jogador.java
+++ src/main/java/com/stefanini/entity/Jogador.java
@@ -5,11 +5,12 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 @Table(schema = "tb_jogador")
 public class Jogador {
 
     @Id
-    @Column(name = "id_jogador")
+    @Column(name = "IdJogador"/*"id_jogador"*/)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -30,5 +31,45 @@ public class Jogador {
     private List<Stefamon> stefamons = new ArrayList<>();
 
     public Jogador() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    public List<Stefamon> getStefamons() {
+        return stefamons;
+    }
+
+    public void setStefamons(List<Stefamon> stefamons) {
+        this.stefamons = stefamons;
     }
 }
--- src/main/java/com/stefanini/resources/JogadorResource.java
+++ src/main/java/com/stefanini/resources/JogadorResource.java
@@ -5,10 +5,13 @@ import com.stefanini.service.JogadorService;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
-@ApplicationPath("/jogador")
+@Path("/jogador")
 public class JogadorResource {
 
     @Inject
